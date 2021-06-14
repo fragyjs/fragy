ccdc08f4887b714d8f6a27433d66a4873c17fabf
BackRunner <dev@backrunner.top>
2021-06-14 18:24:38 +0800

    feat: use queue to execute events

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-await-in-loop */
 const generateList = require('./modules/generateList.js');
 
-const parallel = [generateList];
+const queue = [generateList];
 
-const execute = async () => {
+const executeParallel = async (generators) => {
   await Promise.all(
-    parallel.map((fn) => {
+    generators.map((fn) => {
       return Promise.resolve(
         (async () => {
           await fn();
@@ -12,6 +13,16 @@ const execute = async () => {
       );
     }),
   );
+};
+
+const execute = async () => {
+  for (const item of queue) {
+    if (typeof item === 'object' && Array.isArray(item)) {
+      await executeParallel(item);
+    } else if (typeof item === 'function') {
+      await item();
+    }
+  }
 };
 
 execute();
