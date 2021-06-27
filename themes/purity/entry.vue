75b9835be9563320a6eafe3d8444c3938d7c0098
BackRunner <dev@backrunner.top>
2021-06-27 22:36:03 +0800

    feat: add system theme check

--- themes/purity/entry.vue
+++ themes/purity/entry.vue
@@ -35,6 +35,11 @@ export default {
     document.title = this.$fragy.title;
     // listen events
     this.$bus.$on('color-theme-changed', this.colorThemeChanged);
+    // check system theme
+    const themeMedia = window.matchMedia('(prefers-color-scheme: dark)');
+    if (themeMedia.matches) {
+      this.darkModeEnabled = true;
+    }
     // generate theme
     if (this.$theme.color?.primary && this.$theme.color?.autoGenerate) {
       const style = generateStyle(this.$theme.color.primary);
