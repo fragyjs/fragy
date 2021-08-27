b641f29a1b9480c6fb727e43774bba5587e9bc01
BackRunner <dev@backrunner.top>
2021-08-28 00:20:28 +0800

    feat: compatible with pure github mode

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,22 @@
+const DEFAULT_CONFIG = {
+  title: 'Fragy',
+  subtitle: 'Another fragy site.',
+  icon: '/favicon.ico',
+};
+
+const DEFAULT_FEED_CONFIG = {
+  articles: {
+    path: 'posts',
+    feed: '/data/posts',
+  },
+  articleList: {
+    output: 'listFeed',
+    feed: '/data/listFeed',
+    splitPage: true,
+    pageSize: 10,
+  },
+};
+
 const formatFeed = (feed) => {
   if (feed && !/^https?\/\//.test(feed)) {
     let formatted = feed;
@@ -13,25 +32,14 @@ const formatFeed = (feed) => {
 };
 
 export const formatConfig = (userConfig) => {
-  const config = Object.assign(
-    {
-      title: 'Fragy',
-      subtitle: 'Another fragy site.',
-      icon: '/favicon.ico',
-      articles: {
-        path: 'posts',
-        feed: '/data/posts',
-      },
-      articleList: {
-        output: 'listFeed',
-        feed: '/data/listFeed',
-        splitPage: true,
-        pageSize: 10,
-      },
-    },
-    userConfig,
-  );
-  config.articles.feed = formatFeed(config.articles.feed);
-  config.articleList.feed = formatFeed(config.articleList.feed);
+  const config = Object.assign(Object.create(null), DEFAULT_CONFIG);
+  if (!userConfig.github_repo) {
+    Object.assign(config, DEFAULT_FEED_CONFIG);
+  }
+  Object.assign(config, userConfig);
+  if (!config.github_repo) {
+    config.articles.feed = formatFeed(config.articles.feed);
+    config.articleList.feed = formatFeed(config.articleList.feed);
+  }
   return config;
 };
