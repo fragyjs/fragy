aea0b428a86c225e54a890d559d9bc7d0ab4e27e
BackRunner <dev@backrunner.top>
2021-12-14 23:55:00 +0800

    feat: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   extends: ['alloy', 'alloy/vue', 'prettier'],
-  env: {
-    browser: true,
-  },
   rules: {
     'vue/component-tags-order': 0,
     'vue/order-in-components': 0,
     'vue/component-definition-name-casing': 0,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
   },
 };
