10306f7ef44ac07d3a06a8eee8dc511f6b5904ea
BackRunner <dev@backrunner.top>
2021-12-16 02:14:35 +0800

    fix: yaml extractor

--- src/utils/article.js
+++ src/utils/article.js
@@ -1,14 +1,17 @@
 import YAML from 'yaml';
 
-const yamlExtractor = /^-{3}\r?\n(.+)\r?\n-{3}\r?\n(.*)/s;
+const yamlExtractor = /^-{3}(\r?\n)+([\S\s]+?)\r?\n-{3}(\r?\n)+(.*)$/s;
 
 const parseArticle = (article) => {
   const matches = yamlExtractor.exec(article);
   let meta = null;
-  if (matches && matches.length) {
-    meta = YAML.parse(matches[1]);
+  if (matches.length < 4) {
+    throw new Error('Match content failed.');
   }
-  const content = matches[2].trim();
+  if (matches && matches.length) {
+    meta = YAML.parse(matches[2].trim());
+  }
+  const content = matches[4].trim();
   let abstract;
   if (content.includes('<!-- more -->')) {
     abstract = content.split('<!-- more -->')[0];
@@ -18,7 +21,7 @@ const parseArticle = (article) => {
   return {
     meta,
     abstract,
-    content: matches[2].trim(),
+    content: matches[4].trim(),
   };
 };
 
