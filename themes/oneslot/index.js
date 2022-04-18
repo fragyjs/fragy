5370346ce4a3850642cd172a620afac4ae5334ae
BackRunner <dev@backrunner.top>
2022-04-18 23:00:18 +0800

    feat: import vue-lazyload-next

--- themes/oneslot/index.js
+++ themes/oneslot/index.js
@@ -1,9 +1,15 @@
+import VueLazyload from 'vue-lazyload-next';
 import Landing from './pages/Landing.vue';
 import Article from './pages/Article.vue';
 import './styles/layout.less';
 
 export default {
   name: 'oneslot',
+  setup(app) {
+    app.use(VueLazyload, {
+      observer: true,
+    });
+  },
   routes: [
     {
       path: '/',
