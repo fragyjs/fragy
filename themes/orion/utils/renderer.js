b221ad9f0ebcf9f0fe352a831d25715e4206a73e
BackRunner <dev@backrunner.top>
2022-09-05 00:27:03 +0800

    fix: missing image renderer

--- themes/orion/utils/renderer.js
+++ themes/orion/utils/renderer.js
@@ -11,15 +11,30 @@ renderer.heading = function (text, level) {
       <a name="${escapedText}" class="article-heading-anchor" href="#${escapedText}" data-level="${level}">#</a>
     </h${level}>`.trim();
 };
+
 renderer.text = function (src) {
   return pangu.spacing(src);
 };
+
 renderer.link = function (href, title, text) {
   return `
   <a class="article-link" title="${title}" href="${href}" rel="external nofollow noopener noreferrer">${pangu.spacing(
     text,
   )}</a>
   `.trim();
+};
+
+renderer.image = function (href, title, text) {
+  const cleanedHref = decodeURIComponent(href);
+  let out = `<img data-src="${cleanedHref}"`;
+  if (text) {
+    out += ` alt="${text}"`;
+  }
+  if (title) {
+    out += ` title="${title}"`;
+  }
+  out += '/>';
+  return out;
 };
 
 export default renderer;
